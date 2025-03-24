e9159a26413e0554b4b4aceb9883284563db6a18
mumguy <degenerati228@mail.ru>
2025-03-24 18:30:22 +0300

    mes

--- rocket/Enviroment.cpp
+++ rocket/Enviroment.cpp
@@ -2,10 +2,13 @@
 
 double Enviroment::computeAirDensity(double y) const
 {
-	return rho0 * exp((-m_air * g * y) / (R * T));
+    return rho0 * exp((-m_air * g * y) / (R * T));
 }
 
+
+
 Enviroment::Enviroment(double m_e, double R_e, double rho0, double T, double R, double m_air)
-	: m_e(m_e), R_e(R_e), rho0(rho0), T(T), R(R), m_air(m_air) { }
+    : m_e(m_e), R_e(R_e), rho0(rho0), T(T), R(R), m_air(m_air), rho(rho0) {
+}
 
 
--- rocket/rocket.cpp
+++ rocket/rocket.cpp
@@ -1,12 +1,14 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <cmath>
+#include <vector>
 #include "Enviroment.h"
 #include "FlyingObject.h"
 
 
 const double dt = 0.01;
-
+const double OrderOfRK = 4;
+const int numOfParam = 6;
 
 /*
 class Environment {
@@ -17,7 +19,7 @@ public:
         : G(G), m_e(m_e), R_e(R_e), rho0(rho0), T(T), R(R), m_air(m_air) {}
 
     double computeAirDensity(double y) const {
-        
+
         return rho0 * exp((-m_air * g * y) / (R * T));
     }
 };
@@ -39,7 +41,7 @@ public:
 
     void computeForces(double vx, double vy, double y, double& ax, double& ay, const Environment& env) {
         double rho = env.computeAirDensity(y);
-        
+
 
         ax = (-rho * pow(radius, 2) * pi * shape_coeff * vx + M * U0 * cos(alpha)) / mass;
         ay = (-g * mass - rho * pow(radius, 2) * pi * shape_coeff * vy + M * U0 * sin(alpha)) / mass;
@@ -48,47 +50,165 @@ public:
 
 */
 
-void OneStepRungeKutta(FlyingObject& obj, const Enviroment& env, double dt) {
-
-    double k1vx, k2vx, k3vx, k4vx;
-    double k1vy, k2vy, k3vy, k4vy;
-    double k1x, k2x, k3x, k4x;
-    double k1y, k2y, k3y, k4y;
-    double ax, ay;
-
-    obj.computeForces(obj.vx, obj.vy, obj.y, ax, ay, env);
-
-    k1vx = dt * ax;
-    k1vy = dt * ay;
-    k1x = dt * obj.vx;
-    k1y = dt * obj.vy;
-
-    obj.computeForces(obj.vx + k1vx / 2, obj.vy + k1vy / 2, obj.y + k1y / 2, ax, ay, env);
-    k2vx = dt * ax;
-    k2vy = dt * ay;
-    k2x = dt * (obj.vx + k1vx / 2);
-    k2y = dt * (obj.vy + k1vy / 2);
-
-    obj.computeForces(obj.vx + k2vx / 2, obj.vy + k2vy / 2, obj.y + k2y / 2, ax, ay, env);
-    k3vx = dt * ax;
-    k3vy = dt * ay;
-    k3x = dt * (obj.vx + k2vx / 2);
-    k3y = dt * (obj.vy + k2vy / 2);
-
-    obj.computeForces(obj.vx + k3vx, obj.vy + k3vy, obj.y + k3y, ax, ay, env);
-    k4vx = dt * ax;
-    k4vy = dt * ay;
-    k4x = dt * (obj.vx + k3vx);
-    k4y = dt * (obj.vy + k3vy);
-
-    obj.vx += (k1vx + 2 * k2vx + 2 * k3vx + k4vx) / 6;
-    obj.vy += (k1vy + 2 * k2vy + 2 * k3vy + k4vy) / 6;
-    obj.x += (k1x + 2 * k2x + 2 * k3x + k4x) / 6;
-    obj.y += (k1y + 2 * k2y + 2 * k3y + k4y) / 6;
-
-    obj.mass = obj.mass - obj.M * dt;
-    obj.alpha = atan2(obj.vy, obj.vx);
+double ChooseDeltaT(int i, double dt) {
+    switch (i) {
+    case 0:
+        return 0.0;
+        break;
+    case 1:
+        return dt / 2;
+        break;
+    case 2:
+        return dt / 2;
+        break;
+    case 3:
+        return dt;
+        break;
+    }
 }
+
+
+double bRK(int i) {
+    switch (i) {
+    case 0:
+        return 1.0 / 6;
+        break;
+    case 1:
+        return 2.0 / 6;
+        break;
+    case 2:
+        return 2.0 / 6;
+        break;
+    case 3:
+        return 1.0 / 6;
+        break;
+    }
+}
+
+double cRK(int i) {
+    switch (i) {
+    case 0:
+        return 0.0;
+        break;
+    case 1:
+        return 1.0 / 2;
+        break;
+    case 2:
+        return 1.0 / 2;
+        break;
+    case 3:
+        return 1.0;
+        break;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void oneStepRungeKutt(
+    const std::vector<double>& vecOfLeftParts,
+    std::vector<double>& vecOfNewLeftParts,
+    const std::vector<double(*)(const std::vector<double>& phaseSpace, const FlyingObject& obj, const Enviroment& env)>& vecOfRightParts,
+    const FlyingObject& obj,
+    const Enviroment& env,
+    double t, double dt) {
+    // 
+    std::vector <double> stateVector(numOfParam);
+    std::vector <double> initialStateVector;
+    std::copy(vecOfLeftParts.begin(), vecOfLeftParts.end(), stateVector.begin());
+    std::copy(stateVector.begin(), stateVector.end(), vecOfNewLeftParts.begin());
+    stateVector[stateVector.size() - 1] = t;
+    initialStateVector = stateVector;
+    std::vector<double> vecOfDeltaParam(stateVector.size());
+
+    for (int j = 0; j < OrderOfRK; ++j) {
+        for (int i = 0; i < vecOfDeltaParam.size() - 1; ++i) {
+            vecOfDeltaParam[i] = dt * vecOfRightParts[i](stateVector, obj, env);
+        }
+
+        vecOfDeltaParam[vecOfDeltaParam.size() - 1] = dt;
+
+        for (int i = 0; i < vecOfNewLeftParts.size(); ++i) {
+            vecOfNewLeftParts[i] += vecOfDeltaParam[i] * bRK(j);
+            stateVector[i] = initialStateVector[i] + cRK(j) * vecOfDeltaParam[i];
+        }
+    }
+}
+
+double SecondNewtonLawX(const std::vector<double>& stateVector, const FlyingObject& obj, const Enviroment& env) {
+    double
+        x = stateVector[0],
+        y = stateVector[1],
+        Vx = stateVector[2],
+        Vy = stateVector[3],
+        m = stateVector[4],
+        t = stateVector[5];
+
+    return (-env.rho * pow(obj.radius, 2) * pi * obj.shape_coeff * Vx + obj.M * obj.U0 * cos(atan(Vy / Vx))) / obj.mass;
+}
+
+double SecondNewtonLawY(const std::vector<double>& stateVector, const FlyingObject& obj, const Enviroment& env) {
+    double
+        x = stateVector[0],
+        y = stateVector[1],
+        Vx = stateVector[2],
+        Vy = stateVector[3],
+        m = stateVector[4],
+        t = stateVector[5];
+    return (-g * obj.mass - env.rho * pow(obj.radius, 2) * pi * obj.shape_coeff * Vy + obj.M * obj.U0 * cos(atan(Vy / Vx))) / obj.mass;
+}
+
+double VelocityX(const std::vector<double>& stateVector, const FlyingObject& obj, const Enviroment& env) {
+    double
+        x = stateVector[0],
+        y = stateVector[1],
+        Vx = stateVector[2],
+        Vy = stateVector[3],
+        m = stateVector[4],
+        t = stateVector[5];
+
+    return Vx;
+}
+
+double VelocityY(const std::vector<double>& stateVector, const FlyingObject& obj, const Enviroment& env) {
+    double
+        x = stateVector[0],
+        y = stateVector[1],
+        Vx = stateVector[2],
+        Vy = stateVector[3],
+        m = stateVector[4],
+        t = stateVector[5];
+
+    return Vy;
+}
+
+double massPerTime(const std::vector<double>& stateVector, const FlyingObject& obj, const Enviroment& env) {
+    double
+        x = stateVector[0],
+        y = stateVector[1],
+        Vx = stateVector[2],
+        Vy = stateVector[3],
+        m = stateVector[4],
+        t = stateVector[5];
+
+    double M = obj.M;
+    return M;
+}
+
+void updateState(std::vector<double>& vecOfNewLeftParts,
+    FlyingObject& obj,
+    Enviroment& env) {
+    obj.x = vecOfNewLeftParts[0];
+    obj.y = vecOfNewLeftParts[1];
+    obj.vx = vecOfNewLeftParts[2];
+    obj.vy = vecOfNewLeftParts[3];
+    obj.mass = vecOfNewLeftParts[4];
+
+    obj.alpha = obj.vy / obj.vx;
+    env.rho = env.computeAirDensity(obj.y);
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void WriteInFile(FlyingObject& obj, double t, std::ofstream& file) {
     file << t << "\t" << obj.x << "\t" << obj.y << "\n";
@@ -100,30 +220,40 @@ double TheoreticalTrajectory(double& vy0, double& mass0, double& U0, double& M, 
     return  vy0 + U0 * log10((mass0) / (mass0 - M * t)) / log10(exp(1)) - g * t;
 }
 
-bool СompOfThAndRK(double& vy0, double& mass0, double& U0, double& M, FlyingObject& obj, const Enviroment& env, double& t) {
+bool СompOfThAndRK(double& vy0, double& mass0, double& U0, double& M, FlyingObject& obj, const Enviroment& env, double t) {
     double v_theor = TheoreticalTrajectory(vy0, mass0, U0, M, t);
     return (abs((obj.vy - v_theor) / (v_theor))) <= 0.01;
 }
 
-void CalculationOfTrajectory(FlyingObject& obj, const Enviroment& env, double& t, double dt, std::ofstream& file) {
+void CalculationOfTrajectory(FlyingObject& obj, Enviroment& env, double& t, double dt, std::ofstream& file) {
     double z = 0;
     double vy0 = obj.vy;
     double mass0 = obj.mass;
     double Uy0 = obj.U0;
     double M = obj.M;
     bool Correct = 1;
-    for (;;) {
+    std::vector<double>newState(numOfParam);
+    std::vector<double>State = { obj.x, obj.y, obj.vx, obj.vy, obj.mass };
+    std::vector<double(*)(const std::vector<double>& stateVector, const FlyingObject& obj, const Enviroment& env)> vecOfFunctions =
+    { VelocityX, VelocityY, SecondNewtonLawX, SecondNewtonLawY, massPerTime };
 
-        OneStepRungeKutta(obj, env, dt);
-        if (obj.y <= 0 || obj.mass <= 0) { break; }
+    for (;;) {
+        oneStepRungeKutt(State, newState, vecOfFunctions, obj, env, t, dt);
+        updateState(newState, obj, env);
+        if (obj.y <= 0 || obj.mass <= 0) {
+            obj.y = 0;
+            obj.x = State[0] + ((State[2] / State[3]) * (0 - State[1]));
+            break;
+        }
+        State = newState;
         if (Correct == 1) {
-            Correct = СompOfThAndRK(vy0, mass0, Uy0, M, obj, env, t);
+            Correct = СompOfThAndRK(vy0, mass0, Uy0, M, obj, env, t + dt);
         }
         WriteInFile(obj, t, file);
-        t += dt;
         if (Correct == 0) {
-            СompOfThAndRK(vy0, mass0, Uy0, M, obj, env, t);
+            СompOfThAndRK(vy0, mass0, Uy0, M, obj, env, t + dt);
         }
+        t += dt;
     }
     if (Correct != 1) {
         std::cout << "error > 1% \n";
@@ -131,12 +261,10 @@ void CalculationOfTrajectory(FlyingObject& obj, const Enviroment& env, double& t
     std::cout << "Landed in x: " << obj.x << " m in t: " << t << " s\n";
 }
 
-
-
 int main() {
     const double dt = 0.01;
     Enviroment earth(5.97e24, 6.37e6, 1.23, 237, 8.31, 0.02897);
-    FlyingObject stone(5, 0.2, 0, 100, pi / 4, 0.01, 100);
+    FlyingObject stone(5, 0.2, 0, 100, pi / 4, 0.0, 100);
 
     std::ofstream file("trajectory.txt");
     if (!file) {
@@ -149,7 +277,7 @@ int main() {
     std::cout << "Start  on Earth\n";
     CalculationOfTrajectory(stone, earth, t, dt, file);
 
-
     file.close();
     return 0;
 }
+
